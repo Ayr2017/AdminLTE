168eea0e869484ca7afb0921c36f8e2f42734a97
Ayr2017 <ayrat.2013@yandex.ru>
2019-05-06 08:13:45 +0300

    Stop value

--- pointcontroller.php
+++ pointcontroller.php
@@ -2,7 +2,8 @@
 
 function getPoint($data)
 {
-    if($data) return $data+0.00001;
+    if($data && $data < 37.8007) return $data+0.00001;
+    elseif($data && $data >= 37.8007) return 37.8007;
     return 37.8002;
 }
 echo  getPoint(json_decode($_GET["ltt"]));
